e835b87bc3fe133820a7633615d87bc6330f7648
Taewoong Moon <moondingie5@gmail.com>
2019-12-11 16:41:43 +0900

    메인화면 작업중

--- pages/index.vue
+++ pages/index.vue
@@ -29,9 +29,59 @@
           </div>
         </div>
         <div class="header__bottom">
-          <div class="content-container">1</div>
+          <div class="content-container">
+            <ul class="">
+              <li>
+                <a href="/">
+                  <span>투어&티켓</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span>항공권</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span>숙소</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span>특가여행</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNCIgaGVpZ2h0PSIxNCIgdmlld0JveD0iMCAwIDE0IDE0Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPGNpcmNsZSBjeD0iNyIgY3k9IjciIHI9IjciIGZpbGw9IiNGQTVCNEEiLz4KICAgICAgICA8cGF0aCBmaWxsPSIjRkZGIiBmaWxsLXJ1bGU9Im5vbnplcm8iIGQ9Ik00LjUxMiA5LjVWMy44Mkg1LjhsMi4yNjQgMy41MDQuMDE2LS4wMDhWMy44Mkg5LjRWOS41SDguMDcyTDUuODQ4IDYuMDkyaC0uMDE2VjkuNXoiLz4KICAgIDwvZz4KPC9zdmc+Cg==" alt="">
+                  <span>패키지</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span>여행자보험</span>
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <span>여행준비</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
+      <main class="main">
+        <div>
+          <section class="first">
+            <h2>
+              어디로 떠나세요?
+            </h2>
+            <input type="text" placeholder="여행지나 상품을 검색해보세요!">
+          </section>
+        </div>
+
+      </main>
     </div>
     <main>
       <h1>main</h1>
@@ -46,6 +96,9 @@ export default {
 </script>
 
 <style lang='scss'>
+$white-font: #fff;
+$blue-backgroundcolor: #2b96ed;
+
 html,
 body {
   margin: 0;
@@ -53,8 +106,9 @@ body {
 
 a {
   text-decoration: none;
-}
 
+}
+// ======= header 시작
 .header {
   background-color: #2b96ed;
   width: 100%;
@@ -100,15 +154,74 @@ a {
   }
 
   &__bottom {
-    height: 53px;
+    height: 50px;
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+    li {
+      position: relative;
+      display: inline-block;
+      height: 50px;
+      line-height: 50px;
+    }
+    li:hover {
+      color: red;
+      // border-bottom-color: $white-font;
+      border-bottom: 3px solid red;
+    }
+    a {
+      box-sizing: border-box;
+      height: 50px;
+      color: inherit;
+      margin: 0 12px;
+      span {
+        margin: 0 4px;
+        box-sizing: border-box;
+      }
+      span:first-child {
+        margin-left: 0;
+      }
+      img {
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+    a:first-child {
+      margin-left: 0;
+    }
+    color: $white-font;
+    font-size: 16px;
   }
 }
+// ======= header 끝
+
+// ======= main 시작
+.main {
+  .first {
+    background-color: $blue-backgroundcolor;
+    height: 309px;
+    border-top: 1px solid #1583db;
+    color: $white-font;
+      h2 {
+      margin-top: 56px;
+      margin-bottom: 16px;
+    }
+  }
+}
+
+
+// ======= main 끝
+
+
 
 .content-container {
   margin: 0 102px;
   height: 100%;
-  // border: 1px solid red;
   position: relative;
 }
+
 </style>
 
